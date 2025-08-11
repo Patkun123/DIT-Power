14d6d5b34d8b3a6b141c6cd5c1dbe6a56d26c21f
Your Name <your_email@example.com>
2025-08-11 17:08:14 +0800

    UI Quiz

--- DTI-Power/resources/views/Auth/Users/partials/header.blade.php
+++ DTI-Power/resources/views/Auth/Users/partials/header.blade.php
@@ -128,13 +128,21 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="flex items-center gap-2 py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">
+                            <a href="{{ route('quiz') }}"
+                            class="@if(request()->routeIs('quiz'))
+                                flex items-center gap-2 text-primary-700 font-semibold dark:text-lime-500 bg-primary-200 p-2 lg:bg-white lg:dark:bg-gray-800 lg:p-0
+                            @else
+                                flex items-center gap-2 text-gray-800 lg:bg-white dark:text-white lg:text-primary-700 dark:text-white dark:text-lime-500 p-2 lg:bg-white lg:dark:bg-gray-800 lg:p-0
+                            @endif">
                                 <!-- Health Quiz Icon -->
                                 <svg class="w-[30px] h-[30px] text-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.008-3.018a1.502 1.502 0 0 1 2.522 1.159v.024a1.44 1.44 0 0 1-1.493 1.418 1 1 0 0 0-1.037.999V14a1 1 0 1 0 2 0v-.539a3.44 3.44 0 0 0 2.529-3.256 3.502 3.502 0 0 0-7-.255 1 1 0 0 0 2 .076c.014-.398.187-.774.48-1.044Zm.982 7.026a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2h-.01Z" clip-rule="evenodd"/>
                                 </svg>
 
-                                <span class="text-sm font-sm">Health Quiz</span>
+                                <span class="relative group text-sm">
+                                            Health Quiz
+                                    <span class="absolute left-0 -bottom-1 w-0 h-[2px] bg-current transition-all duration-300 group-hover:w-full"></span>
+                                </span>
                             </a>
                         </li>
                         <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex cursor-pointer items-center gap-2 py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">
