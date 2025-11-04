0622eb82dd59ee26cf575efd27295ad2da2f5307
Your Name <your_email@example.com>
2025-11-04 14:51:14 +0800

    sample

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -10,12 +7,18 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        cors: true,
-    },
     build: {
-            chunkSizeWarningLimit: 1500, // Adjust the limit as needed (in kB)
+    manifest: true,
+    outDir: 'public/build',
+    chunkSizeWarningLimit: 1500, // increases limit from 500 KB to 1.5 MB
+    rollupOptions: {
+        output: {
+            chunkFileNames: 'assets/[name]-[hash].js',
+            entryFileNames: 'assets/[name]-[hash].js',
+            assetFileNames: 'assets/[name]-[hash].[ext]',
         },
-    });
+    },
+},
+
+});
